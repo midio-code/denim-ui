989e137ef2f7e89b9f76014d11d5c3c9f114fcf0
Kristian Hasselknippe <kristian_hasselknippe@outlook.com>
2020-10-16 16:28:22 +0200

    Removed duplicate proc definition

--- src/midio_ui/gui/containers/stack.nim
+++ src/midio_ui/gui/containers/stack.nim
@@ -11,25 +11,20 @@ type
   StackProps* = ref object
     direction*: StackDirection
 
-template cond(cond, a, b: untyped): untyped =
-  if cond:
-    a
-  else:
-    b
 
 proc measureStack(self: Element, props: StackProps, availableSize: Vec2[float]): Vec2[float] =
   var width = 0.0
   var accumulatedHeight = 0.0
   let isVertical = props.direction == StackDirection.Vertical
   for child in self.children:
-    child.measure(cond(isVertical, availableSize.withY(0.0), availableSize.withX(0.0)))
+    child.measure(choose(isVertical, availableSize.withY(0.0), availableSize.withX(0.0)))
     if child.desiredSize.isSome():
-      width = max(width, cond(isVertical, child.desiredSize.get().x, child.desiredSize.get().y))
-      accumulatedHeight += cond(isVertical, child.desiredSize.get().y, child.desiredSize.get().x)
+      width = max(width, choose(isVertical, child.desiredSize.get().x, child.desiredSize.get().y))
+      accumulatedHeight += choose(isVertical, child.desiredSize.get().y, child.desiredSize.get().x)
     else:
       echo "WARN: Child of stack did not have a desired size"
 
-  cond(isVertical, vec2(width, accumulatedHeight), vec2(accumulatedHeight, width))
+  choose(isVertical, vec2(width, accumulatedHeight), vec2(accumulatedHeight, width))
 
 proc arrangeStack(self: Element, props: StackProps, availableSize: Vec2[float]): Vec2[float] =
   var nextPos = vec2(0.0, 0.0)
@@ -39,14 +34,14 @@ proc arrangeStack(self: Element, props: StackProps, availableSize: Vec2[float]):
       child.arrange(
         rect(
           nextPos,
-          cond(
+          choose(
             isVertical,
             vec2(availableSize.x, child.desiredSize.get().y),
             vec2(child.desiredSize.get().x, availableSize.y)
           )
         )
       )
-      nextPos = cond(isVertical, nextPos.addY(child.desiredSize.get().y), nextPos.addX(child.desiredSize.get().x))
+      nextPos = choose(isVertical, nextPos.addY(child.desiredSize.get().y), nextPos.addX(child.desiredSize.get().x))
     else:
       echo "WARN: Child of stack did not have a desired size"
   self.desiredSize.get()
