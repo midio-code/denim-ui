28460501079e0fa05b2689aa4ab8b505a2029764
Kristian Hasselknippe <kristian_hasselknippe@outlook.com>
2020-10-27 18:10:08 +0100

    OnPointer behaviors emits the pointer position as well

--- src/midio_ui/gui/behaviors/onPointer.nim
+++ src/midio_ui/gui/behaviors/onPointer.nim
@@ -5,26 +5,26 @@ import ../../events
 import ../element
 import ../element_events
 
-proc onPressed*(handler: (elem: Element) -> PointerEventResult): Behavior =
+proc onPressed*(handler: (elem: Element, pos: Point) -> PointerEventResult): Behavior =
   # TODO: Remove behavior when unrooted
   Behavior(
     added: some(
       proc(elem: Element):void =
         elem.onPointerPressed(
           proc(arg: PointerArgs): PointerEventResult =
-            handler(elem)
+            handler(elem, arg.pos)
         )
     )
   )
 
-proc onReleased*(handler: (elem: Element) -> PointerEventResult): Behavior =
+proc onReleased*(handler: (elem: Element, pos: Point) -> PointerEventResult): Behavior =
   # TODO: Remove behavior when unrooted
   Behavior(
     added: some(
       proc(elem: Element): void =
         elem.onPointerReleased(
           proc(arg: PointerArgs): PointerEventResult =
-            handler(elem)
+            handler(elem, arg.pos)
         )
     )
   )
