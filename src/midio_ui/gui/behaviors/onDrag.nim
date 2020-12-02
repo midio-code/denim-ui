fbf576841c5457dcfcb29f726b6d1872f2dd8e05
Sebastian Reinhard <sebastian@sebbert.me>
2020-12-02 13:56:54 +0100

    Add canStartDrag parameter to onDrag behavior

--- src/midio_ui/gui/behaviors/onDrag.nim
+++ src/midio_ui/gui/behaviors/onDrag.nim
@@ -1,17 +1,28 @@
 import sugar
 import options
+import rx_nim
 import ../behaviors
 import ../../events
 import ../../vec
 import ../element
 import ../element_events
 
-proc onDrag*(moved: (Vec2[float] -> void), pointerIndex: PointerIndex = PointerIndex.Primary): Behavior =
+proc onDrag*(
+  moved: (Vec2[float] -> void),
+  pointerIndex: PointerIndex = PointerIndex.Primary,
+  canStartDrag: Observable[bool] = behaviorSubject(true)
+): Behavior =
   var pastPos = vec2(0.0, 0.0)
+  var canCurrentlyStartDrag = true
 
   Behavior(
     added: some(
       proc(element: Element): void =
+        discard canStartDrag.subscribe( # TODO: Dispose subscription
+          proc(value: bool) =
+            canCurrentlyStartDrag = value
+        )
+
         element.onPointerMoved(
           proc(arg: PointerArgs): EventResult =
             if element.hasPointerCapture():
@@ -21,7 +32,7 @@ proc onDrag*(moved: (Vec2[float] -> void), pointerIndex: PointerIndex = PointerI
         )
         element.onPointerPressed(
           proc(arg: PointerArgs): EventResult =
-            if arg.pointerIndex == pointerIndex:
+            if canCurrentlyStartDrag and arg.pointerIndex == pointerIndex:
               element.capturePointer()
               pastPos = arg.actualPos
               return handled()
