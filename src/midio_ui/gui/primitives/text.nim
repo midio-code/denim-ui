bd6c4c527426249e43c8d094732a9c4bd6a110b6
Sebastian Reinhard <sebastian@sebbert.me>
2020-12-09 13:35:43 +0100

    Cache text measurements

--- src/midio_ui/gui/primitives/text.nim
+++ src/midio_ui/gui/primitives/text.nim
@@ -1,4 +1,4 @@
-import sugar, options, strutils, sequtils
+import sugar, options, strutils, sequtils, tables, hashes
 import ../element
 import ../types
 import ../drawing_primitives
@@ -19,21 +19,15 @@ type
 
   FontDescriptor = ref object
     font: string
-    fontSize: float
+    fontSize: float # TODO: Avoid using floating point number where we need an exact match
 
-# TODO: Remove need for this global
-var measureText*: (text: string, fontSize: float, font: string, baseline: string) -> Vec2[float]
+  FontMeasureCache = TableRef[string, Vec2[float]]
 
-proc measureToken(token: string, font: FontDescriptor): Vec2[float] =
-  # TODO: Cache
-  # TODO: Adde baseline to TextProps?
-  measureText(token, font.fontSize, font.font, baseline="top")
+proc hash(fd: FontDescriptor): Hash =
+  !$(hash(fd.font) !& hash(fd.fontSize))
 
-iterator tokens(str: string): string =
-  # TODO: Avoid copies by working with ranges
-  for (token, _) in tokenize(str):
-    # TODO: Split consecutive whitespace into separate tokens, so we don't cache every length of whitespace separately
-    yield token
+proc `==`(a: FontDescriptor, b: FontDescriptor): bool =
+  a.font == b.font and a.fontSize == b.fontSize
 
 proc fontDescriptor(props: TextProps): FontDescriptor =
   FontDescriptor(
@@ -41,12 +35,40 @@ proc fontDescriptor(props: TextProps): FontDescriptor =
     fontSize: props.fontSize.get(defaults.fontSize)
   )
 
+let fontMeasureCaches = newTable[FontDescriptor, FontMeasureCache]()
+
+# TODO: Remove need for this global
+var measureText*: (text: string, fontSize: float, font: string, baseline: string) -> Vec2[float]
+
+proc getOrInit[K, V](table: TableRef[K, V], key: K, init: proc(): V): V =
+  if table.hasKey(key): # TODO: Avoid double loookup
+    result = table[key]
+  else:
+    result = init()
+    table[key] = result
+
+proc measureToken(token: string, font: FontDescriptor, cache: FontMeasureCache): Vec2[float] =
+  # TODO: Adde baseline to TextProps?
+  cache.getOrInit(
+    token,
+    proc(): Vec2[float] =
+      measureText(token, font.fontSize, font.font, baseline="top")
+  )
+
+iterator tokens(str: string): string =
+  # TODO: Avoid copies by working with ranges
+  for (token, _) in tokenize(str):
+    # TODO: Split consecutive whitespace into separate tokens, so we don't cache distinct sequences of whitespace separately
+    yield token
+
 method measureOverride(self: Text, avSize: Vec2[float]): Vec2[float] =
   let font = self.textProps.fontDescriptor
-  
+
+  let measureCache = fontMeasureCaches.getOrInit(font, () => newTable[string, Vec2[float]]())
+
   var totalSize = vec2(0.0)
   var lines: seq[TextLine] = @[]
-  
+
   var lineSize = vec2(0.0)
   var lastLineSize = lineSize
   var lineTokens: seq[string] = @[]
@@ -61,7 +83,7 @@ method measureOverride(self: Text, avSize: Vec2[float]): Vec2[float] =
     lineTokens = @[]
 
   for token in self.textProps.text.tokens():
-    let tokenSize = measureToken(token, font)
+    let tokenSize = measureToken(token, font, measureCache)
 
     if lineSize.x + tokenSize.x > avSize.x:
       flushLine()
