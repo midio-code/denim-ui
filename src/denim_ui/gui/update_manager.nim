d9bcb312cf83a37f199dfb4e431b89bc07a82c0b
Kristian Hasselknippe <kristian_hasselknippe@outlook.com>
2021-01-22 14:52:08 +0100

    Added a crude waiting mechanism to update_manager
    That lets you schedule something to happen in the future

--- src/denim_ui/gui/update_manager.nim
+++ src/denim_ui/gui/update_manager.nim
@@ -11,5 +11,36 @@ proc removeUpdateListener*(listener: EventHandler[float]): void =
   if updateManagerListeners.contains(listener):
     updateManagerListeners.remove(listener)
 
+var time = 0.0
+
+type Waiter = object
+  callback: () -> void
+  startedAt: float
+  waitingFor: float
+
+var waiters: seq[Waiter] = @[]
+
+proc wait*(callback: () -> void, waitFor: float): () -> void =
+  let w =
+    Waiter(
+      callback: callback,
+      startedAt: time,
+      waitingFor: waitFor
+    )
+  waiters.add(w)
+  result = proc(): void =
+    let i = waiters.find(w)
+    if i >= 0:
+      waiters.delete(i)
+
 proc dispatchUpdate*(dt: float): void =
+  time += dt
   updateManagerListeners.emit(dt)
+
+  var toRemove: seq[int] = @[]
+  for i, w in waiters.pairs():
+    if time >= w.startedAt + w.waitingFor:
+      w.callback()
+      toRemove.add(i)
+  for i in toRemove:
+    waiters.delete(i)
