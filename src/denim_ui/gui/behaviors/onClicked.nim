73f602d6458f415088a7b8ab03f1e32aa5c5e038
Sebastian Reinhard <sebastian@sebbert.me>
2021-01-25 12:28:52 +0100

    Allow multiple pointer captures per element

--- src/denim_ui/gui/behaviors/onClicked.nim
+++ src/denim_ui/gui/behaviors/onClicked.nim
@@ -2,23 +2,30 @@ import sugar
 import options
 import ../behaviors
 import ../../events
+import ../../guid
 import ../element
 import ../element_events
 
 proc onClicked*(handler: (elem: Element, args: PointerArgs) -> void): Behavior =
-  var pressed = false
   Behavior(
     added: some(
       proc(element: Element): void =
+        let id = genGuid()
+        var pressed = false
+
+        proc onLostCapture() =
+          pressed = false
+
         element.onPointerPressed(
           proc(arg: PointerArgs): EventResult =
-            pressed = true
+            pressed = element.capturePointer(id, CaptureKind.Soft, onLostCapture)
         )
         element.onPointerReleased(
           proc(args: PointerArgs): EventResult =
-            if pressed and not element.pointerCapturedBySomeoneElse:
+            if pressed:
+              element.releasePointer(id)
+              pressed = false
               handler(element, args)
-            pressed = false
         )
     )
   )
