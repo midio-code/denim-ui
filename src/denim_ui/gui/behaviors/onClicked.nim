7a47cc71d0accc4e8b123a5d1875672f242850f3
Kristian Hasselknippe <kristian_hasselknippe@outlook.com>
2021-03-06 16:12:18 +0100

    Trynna resolve conflicting click behaviors

--- src/denim_ui/gui/behaviors/onClicked.nim
+++ src/denim_ui/gui/behaviors/onClicked.nim
@@ -8,7 +8,7 @@ import ../../guid
 import ../../events
 
 type
-  ClickedHandler* = (Element, PointerArgs) -> void
+  ClickedHandler* = (Element, PointerArgs, var EventResult) -> void
 
 let behaviorId = genGuid()
 
@@ -23,6 +23,7 @@ proc onClicked*(handler: ClickedHandler): Behavior =
 
           proc onLostCapture() =
             pressed = false
+            echo "Clicked lost capture"
 
           element.onPointerPressed(
             proc(arg: PointerArgs, res: var EventResult): void =
@@ -33,7 +34,7 @@ proc onClicked*(handler: ClickedHandler): Behavior =
             proc(args: PointerArgs, res: var EventResult): void =
               if pressed and not res.isHandledBy(behaviorId):
                 for handler in clickedHandlers[element]:
-                  handler(element, args)
+                  handler(element, args, res)
                 res.addHandledBy(behaviorId)
               pressed = false
           )
