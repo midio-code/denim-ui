a13449012a52dab03e141060484896e4b0afef65
Kristian Hasselknippe <kristian_hasselknippe@outlook.com>
2021-06-03 14:33:40 +0200

    Made observeWorldPosition more accurate

--- src/denim_ui/gui/world_position.nim
+++ src/denim_ui/gui/world_position.nim
@@ -1,16 +1,10 @@
-import types, tables, ../vec, rx_nim, options
+import types, tables, ../vec, rx_nim, options, sugar
 
 # TODO: This is probably not a robust way to cache this, and is just a
 # proof of concept for caching values that depends on layout calculations
 var worldPositions = initTable[Element, Vec2[float]]()
 
-var worldPositionObservers = initTable[Element, Subject[Point]]()
-
-proc actualWorldPosition*(self: Element): Vec2[float] =
-  worldPositions.mgetorput(self, zero())
-
-proc observeWorldPosition*(self: Element): Observable[Vec2[float]] =
-  worldPositionObservers.mgetorput(self, subject[Vec2[float]]()).unique
+var worldPositionObservers = initTable[Element, Subject[Option[Point]]]()
 
 proc recalculateWorldPositionsCache*(root: Element): void =
   worldPositions.clear()
@@ -19,7 +13,19 @@ proc recalculateWorldPositionsCache*(root: Element): void =
       let elemPos = elem.bounds.get().pos + parentPos
       worldPositions[elem] = elemPos
       if elem in worldPositionObservers:
-        worldPositionObservers[elem] <- elemPos
+        worldPositionObservers[elem] <- some(elemPos)
       for c in elem.children:
         c.calcWorldPos(elemPos)
   root.calcWorldPos(zero())
+
+proc actualWorldPosition*(self: Element): Vec2[float] =
+  if self in worldPositions:
+    return worldPositions[self]
+  else:
+    result = self.bounds.map((b: Bounds) => b.pos).get(zero()) + self.parent.map(p => p.actualWorldPosition).get(zero())
+    worldPositions[self] = result
+    if self in worldPositionObservers:
+      worldPositionObservers[self] <- some(result)
+
+proc observeWorldPosition*(self: Element): Observable[Point] =
+  worldPositionObservers.mgetorput(self, behaviorSubject[Option[Point]](some(self.actualWorldPosition))).source.unwrap.unique
