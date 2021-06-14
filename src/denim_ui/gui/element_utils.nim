d0515898a98307123e2a5231922e695af8f4609d
Kristian Hasselknippe <kristian_hasselknippe@outlook.com>
2021-06-14 16:56:06 +0200

    Added util proc `isAncestorOf(Element, Element) -> bool`

--- src/denim_ui/gui/element_utils.nim
+++ src/denim_ui/gui/element_utils.nim
@@ -6,3 +6,11 @@ proc bringToFront*(self: Element): void =
     let largestZIndex = self.parent.get().children.foldl(max(a, b.props.zIndex.get(0)), low(int))
     # TODO: Do not increase z index if self is already in front
     self.props.zIndex = some(largestZIndex + 1)
+
+proc isAncestorOf*(ancestor: Element, ofElem: Element): bool =
+  var current = ofElem
+  while current.parent.isSome:
+    current = current.parent.get
+    if current == ancestor:
+      return true
+  return false
